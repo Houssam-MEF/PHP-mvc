37e512654b1c31256a6d18b3728eb15851620135
Houssam-MEF <houssam.mh@outlook.com>
2024-05-14 14:28:20 +0100

    MVC Url Redirection

--- accueil.php
+++ accueil.php
@@ -1,11 +1,14 @@
+<?php
+$URL = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+?>
 <center>
-<a href="visiteur">
+<a href="<?=$URL?>visiteur">
     Gestion des visiteurs
 </a> / 
-<a href="commande">
+<a href="<?=$URL?>commande">
     Gestion des commandes
 </a>  / 
-<a href="produit">
+<a href="<?=$URL?>produit">
     Gestion des produits
 </a>
 
@@ -26,7 +29,6 @@ if(count($data_url)>1)
    if(count($data_url)>2)
       $id=$data_url[2];
 }
-
 }
  $controleur=ucfirst(strtolower($controleur));
 if(in_array($controleur,$Tableau_controleurs))
